1f3ea0214eebdc1c00e9a5c10c6a1937d5d02cf6
Jason Beck <mrbass21@gmail.com>
2025-03-28 16:34:03 -0500

    Completed Getting Started from the web tutorial

--- client.c
+++ client.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <wayland-client.h>
 
-
 struct wl_display *display = NULL;
+struct wl_registry *registry = NULL;
+struct wl_compositor *compositor = NULL;
+
+static void global_registry_handler(void *data, struct wl_registry *registry, uint32_t id, const char *interface, uint32_t version) {
+    printf("Got a registry event for <%s>, id: %d, version: %d\n", interface, id, version);
+
+    if(strcmp(interface, "wl_compositor") == 0) {
+        compositor = wl_registry_bind(registry, id, &wl_compositor_interface, version);
+    }
+}
+
+static void global_registry_remove_handler(void *data, struct wl_registry *registry, uint32_t id) {
+    printf("Got a registry losing event for <%d>\n", id);
+}
+
+const static struct wl_registry_listener registry_listener = {
+    .global = global_registry_handler,
+    .global_remove = global_registry_remove_handler
+};
 
 int main(int argc, char *argv[]) {
     display = wl_display_connect(NULL);
@@ -13,7 +32,18 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    printf("Connected to a display!\n");
+    registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+
+    wl_display_dispatch(display);
+    wl_display_roundtrip(display);
+
+    if(compositor == NULL) {
+        fprintf(stderr, "Could not find a compositor.\n");
+        exit(1);
+    } else {
+        printf("Found a compositor!\n");
+    }
 
     wl_display_disconnect(display);
     printf("Disconnected from the display!\n");
